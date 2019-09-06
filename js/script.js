af9cd6b67ded892daee1203d8e71919e62efca87
DebarunMitra <debarun96mitra@gmail.com>
2019-09-06 17:56:01 +0530

    Version 1.0
    All content added

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ Author: Debarun Mitra
 Technology Used:HTML,css,javaScript,Bootstrap,jQuery
 Objective: Create a smartwatch which able to show time, check message, play music and has a stopwatch
 */
-//const songs=["Absolute_Night.mp3","In_my_City.mp3","working.mp3"];
+/*music player content start*/
 const songs=[
   {song:"media/Absolute_Night.mp3",name:"Absolute_Night",singer:"Unknown1",poster:"media/Poster1.jpg"},
   {song:"media/In_my_City.mp3",name:"In_my_City",singer:"Unknown2",poster:"media/Poster2.jpg"},
@@ -12,24 +12,40 @@ const songs=[
 let songName=document.getElementById("songName");
 let singerName=document.getElementById("singerName");
 let fillBar = document.getElementById("fill");
-  let song=new Audio();
-    let currentSong=1;
+let song=new Audio();
+let currentSong=1;
 function playSong(){
     song.src=songs[currentSong].song;
     songName.textContent=songs[currentSong].name;
     singerName.textContent=songs[currentSong].singer;
-    //console.log(songName);
-    //song.play();
+    document.getElementById("poster").src=songs[currentSong].poster;
 }
-playSong();
-function clicker()
-{
-  alert("ok button");
-}
-
-//playSong();
+/*music player content stop*/
 $(document).ready(function(){
-  //alert("ok");
+        /*message start*/
+  $("#msgBtn").click(function(){
+  console.log('message');
+    $("#musicBtn").css("background-color","#373762");
+      $("#msgBtn").css("background-color","#00FFCC");
+        $("#swBtn").css("background-color","#373762");
+        $("#musicMainDiv").css("display","none");
+        $("#musicMainDiv").children().hide();
+        $("#swMainDiv").css("display","none");
+        $("#msgMainDiv").css("display","block");
+  });
+      /*message stop*/
+    /*music player start*/
+$("#musicBtn").click(function(){
+console.log('music');
+  $("#musicBtn").css("background-color","#00FFCC");
+    $("#msgBtn").css("background-color","#373762");
+      $("#swBtn").css("background-color","#373762");
+      $("#swMainDiv").css("display","none");
+      $("#msgMainDiv").css("display","none");
+      $("#musicMainDiv").css("display","block");
+      $("#musicMainDiv").children().show();
+playSong();
+});
   $("#musicPause").click(function(){
     $("#musicPause").css("display","none");
         $("#musicPlay").css("display","block");
@@ -67,4 +83,18 @@ $(document).ready(function(){
     let position=song.currentTime / song.duration;
     fillBar.style.width = position * 100 +'%';
   });*/
+  /*music player end*/
+
+  /*message start*/
+$("#swBtn").click(function(){
+console.log('stopwatch');
+$("#musicBtn").css("background-color","#373762");
+$("#msgBtn").css("background-color","#373762");
+$("#swBtn").css("background-color","#00FFCC");
+$("#musicMainDiv").css("display","none");
+$("#musicMainDiv").children().hide();
+$("#msgMainDiv").css("display","none");
+$("#swMainDiv").css("display","block");
+});
+/*message stop*/
 });
