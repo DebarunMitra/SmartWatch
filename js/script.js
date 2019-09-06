6ff48c847c9e124e0ea4c92c395395f47d457da9
DebarunMitra <debarun96mitra@gmail.com>
2019-09-06 12:35:31 +0530

    Version 1.0
    js file added and other designs are done

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/*
+Author: Debarun Mitra
+Technology Used:HTML,css,javaScript,Bootstrap,jQuery
+Objective: Create a smartwatch which able to show time, check message, play music and has a stopwatch
+*/
