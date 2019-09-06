c2478737d2c67923474d4819f704ea0c67cfa5f4
DebarunMitra <debarun96mitra@gmail.com>
2019-09-06 20:07:48 +0530

    Version 1.0
    stopwatch added

--- js/script.js
+++ js/script.js
@@ -12,6 +12,17 @@ const songs=[
 let songName=document.getElementById("songName");
 let singerName=document.getElementById("singerName");
 let fillBar = document.getElementById("fill");
+let mins = document.getElementById("min");
+let  secs = document.getElementById("sec");
+let  cents = document.getElementById("cent");
+let  start=document.getElementById("start");
+let  stop=document.getElementById("stop");
+let  reset=document.getElementById("reset");
+let  lap=document.getElementById("lap");
+let currentTimer = 0, interval = 0, lastUpdateTime = new Date().getTime();
+ //mins.innerHTML="11";
+ //cents.innerHTML=12;
+ //secs.innerHTML=25;
 let song=new Audio();
 let currentSong=1;
 function playSong(){
@@ -21,7 +32,49 @@ function playSong(){
     document.getElementById("poster").src=songs[currentSong].poster;
 }
 /*music player content stop*/
+/*stopwatch start*/
+function startTimer () {
+        if (!interval) {
+            lastUpdateTime = new Date().getTime();
+            interval = setInterval(update, 1);
+        }
+    }
+function stopTimer (){
+        clearInterval(interval);
+        interval = 0;
+    }
+function resetTimer (){
+        stopTimer();
+        currentTimer = 0;
+        mins.innerHTML = secs.innerHTML = cents.innerHTML = pad(0);
+    }
+function pad (n) {
+        return ('00' + n).substr(-2);
+    }
+    function update () {
+    let now = new Date().getTime(),
+        dt = now - lastUpdateTime;
+        currentTimer += dt;
+        let time = new Date(currentTimer);
+      //  console.log(pad(time.getMinutes()));
+        //console.log(pad(time.getSeconds()));
+        //console.log(pad(Math.floor(time.getMilliseconds() / 10)));
+      //  mins.innerHTML = pad(time.getMinutes());
+        secs.innerHTML = pad(time.getSeconds());
+        cents.innerHTML = pad(Math.floor(time.getMilliseconds() / 10));
+        lastUpdateTime = now;
+    }
+/*stopwatch stop*/
 $(document).ready(function(){
+  $("#start").click(function(){
+    startTimer();
+  });
+  $("#stop").click(function(){
+    stopTimer();
+  });
+  $("#reset").click(function(){
+    resetTimer();
+  });
         /*message start*/
   $("#msgBtn").click(function(){
   console.log('message');
