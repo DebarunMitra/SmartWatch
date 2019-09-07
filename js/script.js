4f308522624acdcbb9bf651309bbcceed0cbf8b3
DebarunMitra <debarun96mitra@gmail.com>
2019-09-07 08:55:04 +0530

    Version 1.0
    message,music,timer added

--- js/script.js
+++ js/script.js
@@ -5,17 +5,14 @@ Objective: Create a smartwatch which able to show time, check message, play musi
 */
 /*music player content start*/
 const songs=[
-  {song:"media/Absolute_Night.mp3",name:"Absolute_Night",singer:"Unknown1",poster:"media/Poster1.jpg"},
-  {song:"media/In_my_City.mp3",name:"In_my_City",singer:"Unknown2",poster:"media/Poster2.jpg"},
-  {song:"media/working.mp3",name:"working",singer:"Unknown3",poster:"media/Poster3.jpg"}
+  {song:"media/Absolute_Night.mp3",name:"Absolute_Night",singer:"Unknown",poster:"media/Poster1.jpg"},
+  {song:"media/In_my_City.mp3",name:"In_my_City",singer:"Unknown",poster:"media/Poster2.jpg"},
+  {song:"media/working.mp3",name:"worKing",singer:"Unknown",poster:"media/Poster3.jpg"}
 ];
 const message=[
-  {id:1,sender:"Arjun",msg:"Hi Debarun,1 Call me when you will free.",color:"#ff6666"},
-  {id:2,sender:"Robin",msg:"Hi Debarun,2 Call me when you will free..",color:"#99ff99"},
-  {id:3,sender:"John",msg:"Hi Debarun,3 Call me when you will free..",color:"#ff80df"},
-  {id:4,sender:"Alphaso",msg:"Hi Debarun,4 Call me when you will free..",color:"#ff80df"},
-  {id:5,sender:"Bitas",msg:"Hi Debarun,5 Call me when you will free..",color:"#ff6666"},
-  {id:6,sender:"Gamandro",msg:"Hi Debarun,6 Call me when you will free..",color:"#b3d9ff"}
+  {id:1,sender:"Arjun",msg:"Hi Debarun, Good morning.",color:"#ff6666"},
+  {id:2,sender:"Robin",msg:"Hi Debarun, Call me when you will free..",color:"#99ff99"},
+  {id:3,sender:"John",msg:"Hi Debarun, Call me when you will free..",color:"#ff80df"}
 ];
 let songName=document.getElementById("songName");
 let singerName=document.getElementById("singerName");
@@ -27,7 +24,7 @@ let start=document.getElementById("start");
 let stop=document.getElementById("stop");
 let reset=document.getElementById("reset");
 let lap=document.getElementById("lap"),
-currentTimer = 0, interval = 0, lastUpdateTime = new Date().getTime(),
+currentTimer = 0, interval = 0, lastUpdateTime = new Date().getTime(),count=0,
 ele = document.querySelector('.msg-content');
 ele.innerHTML = ele.innerHTML.replace(/,/g, ',<br/>')
 let song=new Audio();
@@ -35,16 +32,13 @@ let currentSong=1;
 function msgBox(v){
 let msgFilter=message.filter((item) => item.id===v).map((v,k)=>v).forEach((v,k)=>
 {
-//  console.log(v.sender);
 document.getElementById("msgMainDiv").style.display='none';
-document.getElementById("msg-read").style.display='block';
-//document.getElementById('micon').innerHTML=v.sender;
-document.getElementById("mname").innerHTML=v.sender;
-//document.getElementById('micon').backgroundColor=v.color;
+document.getElementById("msgRead").style.display='block';
+document.getElementById("msgName").innerHTML=v.sender;
+document.getElementById("msgIcon").backgroundColor=v.color;
 document.getElemetById("msgContent").innerHTML=v.msg;
 }
 );
-/**/
 }
 function playSong(){
     song.src=songs[currentSong].song;
@@ -77,33 +71,50 @@ function pad (n) {
         dt = now - lastUpdateTime;
         currentTimer += dt;
         let time = new Date(currentTimer);
-        mins.innerHTML = pad(time.getMinutes());
+        //mins.innerHTML = pad(time.getMinutes());
         secs.innerHTML = pad(time.getSeconds());
-        //cents.innerHTML = pad(Math.floor(time.getMilliseconds() / 10));
+        cents.innerHTML = pad(Math.floor(time.getMilliseconds() / 10));
         lastUpdateTime = now;
     }
 /*stopwatch stop*/
 $(document).ready(function(){
+          $("#content-title > .title").html();
+          $("#content-title > .time").html();
   $("#start").click(function(){
+    $("#start").css("display","none");
+    $("#stop").css("display","block");
     startTimer();
   });
   $("#stop").click(function(){
+    $("#start").css("display","block");
+    $("#stop").css("display","none");
     stopTimer();
   });
   $("#reset").click(function(){
+    $("#start").css("display","block");
+    $("#stop").css("display","none");
+  document.getElementById("lapCount").innerHTML='LAP&nbsp;:&nbsp;'+0;
+  count=0;
     resetTimer();
+  });
+  $("#lap").click(function(){
+    count=count+1;
+    document.getElementById("lapCount").innerHTML='LAP&nbsp;:&nbsp;'+count;
   });
         /*message start*/
   $("#msgBtn").click(function(){
-  console.log('message');
-    $("#musicBtn").css("background-color","#373762");
-      $("#msgBtn").css("background-color","#00FFCC");
+        $("#msgRow").empty();
+        $("#content-title > .title").html("MESSAGE");
+        $("#musicBtn").css("background-color","#373762");
+        $("#msgBtn").css("background-color","#00FFCC");
         $("#swBtn").css("background-color","#373762");
         $("#musicMainDiv").css("display","none");
         $("#musicMainDiv").children().hide();
+        $("#main").css("display","none");
+        $("#image").css("display","none");
+        $("#player").css("display","none");
         $("#swMainDiv").css("display","none");
         $("#msgMainDiv").css("display","block");
-        $("#bottomBtnScroll").css("display","block");
         $(".message-read").css("display","none");
      $.each(message,function(index,value){
         if(index<3)
@@ -112,7 +123,7 @@ $(document).ready(function(){
           +'<p class="msg-icon" style="width:30px;background-color:'+value.color+';">'+
           '<span style="margin-left:30%;">'+value.sender.charAt(0)+'</span>'+
           '</p >'+'</th>'+'<td style="width:100px;">'+
-          '<p class="msg-list-name" onclick="msgBox('+value.id+')">'+value.sender+'</p>'+'</td>'+'</tr>';
+          '<p class="msg-list-name" style="cursor:pointer;" onclick="msgBox('+value.id+')">'+value.sender+'</p>'+'</td>'+'</tr>';
           $('#msgRow').append(row).last();
         }
       });
@@ -120,14 +131,18 @@ $(document).ready(function(){
       /*message stop*/
     /*music player start*/
 $("#musicBtn").click(function(){
-console.log('music');
+  $("#content-title > .title").html("MUSIC");
   $("#musicBtn").css("background-color","#00FFCC");
     $("#msgBtn").css("background-color","#373762");
       $("#swBtn").css("background-color","#373762");
       $("#swMainDiv").css("display","none");
       $("#msgMainDiv").css("display","none");
       $("#musicMainDiv").css("display","block");
+      $(".message-read").css("display","none");
       $("#musicMainDiv").children().show();
+      $("#main").css("display","block");
+      $("#image").css("display","block");
+      $("#player").css("display","block");
 playSong();
 });
   $("#musicPause").click(function(){
@@ -168,10 +183,9 @@ playSong();
     fillBar.style.width = position * 100 +'%';
   });
   /*music player end*/
-
-  /*message start*/
+  /*stopwatch start*/
 $("#swBtn").click(function(){
-console.log('stopwatch');
+$("#content-title > .title").html("TIMER");
 $("#musicBtn").css("background-color","#373762");
 $("#msgBtn").css("background-color","#373762");
 $("#swBtn").css("background-color","#00FFCC");
@@ -179,6 +193,10 @@ $("#musicMainDiv").css("display","none");
 $("#musicMainDiv").children().hide();
 $("#msgMainDiv").css("display","none");
 $("#swMainDiv").css("display","block");
+$(".message-read").css("display","none");
+$("#main").css("display","none");
+$("#image").css("display","none");
+$("#player").css("display","none");
 });
-/*message stop*/
+/*stopwatch stop*/
 });
